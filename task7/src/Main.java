7ae897de9dffc4b9bc04cdb826cf76a46c25e1d6
Maxim <maksimmustajkin@mail.ru>
2025-03-12 22:54:40 +0300

    Read

---
+++ task7/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<User> users = new ArrayList<>();
+
+        // Циклический ввод данных 5 пользователей
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Введите имя пользователя " + i + ": ");
+            String name = scanner.nextLine();
+            System.out.print("Введите возраст пользователя " + i + ": ");
+            Integer age = scanner.nextInt();
+            scanner.nextLine();
+            users.add(new User(name, age));
+        }
+
+        users.sort(new Comparator<User>() {
+            @Override
+            public int compare(User u1, User u2) {
+                return u1.getAge().compareTo(u2.getAge());
+            }
+        });
+
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        scanner.close();
+    }
+}
---
+++ task8/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<Integer, List<User>> userMap = new HashMap<>();
+
+        // Циклический ввод данных 5 пользователей
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Введите имя пользователя " + i + ": ");
+            String name = scanner.nextLine();
+            System.out.print("Введите возраст пользователя " + i + ": ");
+            int age = scanner.nextInt();
+            scanner.nextLine(); // Очистка буфера
+
+            User user = new User(name, age);
+
+            userMap.computeIfAbsent(age, k -> new ArrayList<>()).add(user);
+        }
+
+        System.out.print("Введите требуемый возраст для поиска пользователей: ");
+        Integer searchAge = scanner.nextInt();
+
+        if (userMap.containsKey(searchAge)) {
+            List<User> usersByAge = userMap.get(searchAge);
+
+            usersByAge.sort(Comparator.comparing(User::getName));
+
+            System.out.println("Пользователи с возрастом " + searchAge + ":");
+            for (User user : usersByAge) {
+                System.out.println(user);
+            }
+        } else {
+            System.out.println("Пользователи с возрастом " + searchAge + " не найдены.");
+        }
+
+        scanner.close();
+    }
+}
